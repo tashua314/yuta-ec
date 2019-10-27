fc5d89a15c90fb6584a0e666939e2446b9a45692
Yuta Takahashi <m1205123ing@gmail.com>
2019-10-27 15:46:10 +0900

    tweak

--- public/wp-content/plugins/index.php
+++ public/wp-content/plugins/index.php
@@ -1,19 +1,17 @@
 <?php
 /*
-	Plugin Name: Yuta's pluglins
-	Plugin URI: TODO
-	Description: カスタムプラグイン
-	Author: Yuta Takahashi
-	Version: 1.0
-	Author URI: TODO
-*/
-
+   Plugin Name: Yuta's pluglins
+   Plugin URI: TODO
+   Description: カスタムプラグイン
+   Author: Yuta Takahashi
+   Version: 1.0
+   Author URI: TODO
+ */
 /**
  * ナビゲーションメニューの有無を確認
  *
  */
 function wp_nav_menu_exist( $args = array() ) {
-  $menu = wp_nav_menu($args);
-
-  return trim($menu);
+	$menu = wp_nav_menu($args);
+	return trim($menu);
 }
